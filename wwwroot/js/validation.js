f60b69d2cb36b1b12c76e75b8dca48e00d631880
cepeasbhy <asbhycepe01@gmail.com>
2024-02-12 16:56:03 +0800

    Added additional validations

--- wwwroot/js/validation.js
+++ wwwroot/js/validation.js
@@ -1,20 +1,8 @@
-﻿function ValidateRequestForm() {
-    $("#order-form").validate({
-        rules: {
-            name: "required",
-            quantity: {
-                required: true,
-                number: true
-            },
-            uomId: "required"
-        },
-        messages: {
-            name: "Enter the name of the product",
-            quantity: {
-                required: "Specify the quantity needed",
-                number: "Please input a valid number"
-            },
-            uomId: "Select a unit of measurement" 
-        }
-    });
-}
+﻿window.addEventListener("DOMContentLoaded", () => {
+   
+    
+
+    
+
+    
+});
