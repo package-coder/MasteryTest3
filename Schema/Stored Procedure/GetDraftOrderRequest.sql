30398bcae5dbd03bc5c0f1ea022233b7c112f0f8
Christian <pujado.chris@gmail.com>
2024-02-14 13:04:18 +0800

    Update sp schema

---
+++ Schema/Stored Procedure/GetDraftOrderRequest.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE GetDraftOrderRequest (@clientId INT)
+AS BEGIN
+    SELECT
+        [Order].Id,
+        clientId,
+        crc,
+        status,
+        totalItems,
+        dateOrdered,
+        datePrinted,
+        orderItem.Id,
+        orderItem.orderId,
+        orderItem.productId,
+        orderItem.name,
+        orderItem.quantity,
+        orderItem.remark,
+        UOM.Id,
+        UOM.name,
+        UOM.unit
+    FROM dbo.[Order]
+    LEFT JOIN OrderItem ON OrderItem.orderId = [Order].Id
+    LEFT JOIN UOM ON OrderItem.uom = UOM.Id
+    WHERE clientId = @clientId AND status = 'DRAFT'
+END
