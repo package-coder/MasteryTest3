8ce66c4c76b2cdd2b601dfe6280e46f8295500e9
Christian <pujado.chris@gmail.com>
2024-02-26 16:46:40 +0800

    Update store procedure

--- Schema/Stored Procedure/GetAllOrders.sql
+++ Schema/Stored Procedure/GetAllOrders.sql
@@ -9,34 +9,37 @@ AS BEGIN
         ord.clientId,
         ord.crc,
         ord.status,
-        COUNT(ord.Id) as totalItems,
+        (
+            SELECT COUNT(*)
+            FROM OrderItem orderItem
+            WHERE orderItem.orderId = ord.Id
+        ) AS 'totalItems',
         ord.dateOrdered,
         ord.datePrinted,
+        appUser.Id,
+        appUser.name,      
         orderItem.Id,
         orderItem.orderId,
         orderItem.name,
         orderItem.quantity,
         orderItem.unit,
         orderItem.remark,
-        product.Id,
+        product.Id, 
         product.name
 	FROM [Order] ord
     JOIN OrderItem orderItem ON ord.Id = orderItem.orderId
+    JOIN AppUser appUser ON appUser.Id = ord.clientId
     FULL OUTER JOIN Product product ON orderItem.productId = product.Id	
 	WHERE
 	    (@clientId IS NULL OR ord.clientId = @clientId) AND
 	    (@status IS NULL OR ord.status = @status) AND
 	    (@visibilityLevel IS NULL OR ord.visibilityLevel = @visibilityLevel) AND
 	    ord.dateDeleted IS NULL
-    GROUP BY ord.Id,
-             ord.clientId,
-             ord.crc,
-             ord.status,
-             ord.dateOrdered,
-             ord.datePrinted,
-             orderItem.Id,
-             orderItem.orderId,
-             orderItem.name,
+END;
+
+
+
+      orderItem.name,
              orderItem.quantity,
              orderItem.unit,
              orderItem.remark,
--- Schema/Stored Procedure/GetOrderById.sql
+++ Schema/Stored Procedure/GetOrderById.sql
@@ -1,29 +1,36 @@
 CREATE PROCEDURE GetOrderById(
-	@id INT
+	@id INT,
+    @clientId INT = NULL
 )
 AS BEGIN
-       SELECT
-         ord.Id,
-         ord.crc,
-         ord.[status],
-         ord.totalItems,
-         ord.dateOrdered,
-         ord.datePrinted,
-         appUser.Id,
-         appUser.name,
-         appUser.email,
-         appUser.[address],
-         orderItem.Id,
-         orderItem.orderId,
-         orderItem.productId,
-         orderItem.name,
-         orderItem.quantity,
-         orderItem.remark,
-         orderItem.unit
-     FROM
-         [Order] ord
-             JOIN AppUser appUser ON appUser.id = ord.clientId
-             JOIN OrderItem orderItem ON orderItem.orderId = ord.Id
-     WHERE ord.Id = @id
+    SELECT
+        ord.Id,
+        ord.crc,
+        ord.[status],
+        (
+            SELECT COUNT(*)
+            FROM OrderItem orderItem
+            WHERE orderItem.orderId = ord.Id
+        ) AS 'totalItems',
+        ord.dateOrdered,
+        ord.datePrinted,
+        appUser.Id,
+        appUser.name,
+        orderItem.Id,
+        orderItem.orderId,
+        orderItem.name,
+        orderItem.unit,
+        orderItem.quantity,
+        orderItem.remark,
+        product.Id,
+        product.name
+    FROM
+        [Order] ord
+            JOIN OrderItem orderItem ON orderItem.orderId = ord.Id
+            JOIN AppUser appUser ON appUser.Id = ord.clientId
+            FULL OUTER JOIN Product product ON orderItem.productId = product.Id
+    WHERE ord.Id = @id AND (@clientId IS NULL OR ord.clientId = @clientId)
 END;
+
+
 
