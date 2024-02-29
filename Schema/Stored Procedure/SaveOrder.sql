fa46d29b2ebd9b20a25a3b185e6fbd437fec711a
cepeasbhy <asbhycepe01@gmail.com>
2024-02-29 10:41:10 +0800

    Added for displaying attachment and download receipt in details page.

--- Schema/Stored Procedure/SaveOrder.sql
+++ Schema/Stored Procedure/SaveOrder.sql
@@ -5,13 +5,14 @@ CREATE PROCEDURE [dbo].[SaveOrder]
     @status VARCHAR(20) = 'DRAFT',
     @visibilityLevel INT = NULL,
 	@attachment VARCHAR(MAX) = NULL,
-	@crc INT = NULL
+	@crc INT NULL,
+	@totalItems INT
 ) 
 AS BEGIN
     IF (@Id IS NULL) BEGIN 
         INSERT INTO [Order](clientId, totalItems, [status], attachment)
 		OUTPUT INSERTED.Id
-        VALUES(@clientId, 0, @status, @attachment)
+        VALUES(@clientId, @totalItems, @status, @attachment)
     END ELSE 
     BEGIN 
         DECLARE @maxLevel INT; 
@@ -24,10 +25,11 @@ AS BEGIN
             SET 
 			[status] = @status, dateOrdered = GETDATE(),
 			crc = @crc,
-			attachment = @attachment
+			attachment = @attachment,
+			totalItems = @totalItems
             OUTPUT INSERTED.Id WHERE Id = @Id
         ELSE
-            UPDATE [Order] SET [status] = @status, attachment = @attachment OUTPUT INSERTED.Id WHERE Id = @Id
+            UPDATE [Order] SET [status] = @status, attachment = @attachment, totalItems = @totalItems OUTPUT INSERTED.Id WHERE Id = @Id
     END
 END
 
